0af6938f884ec504e144b8b70297c5fee9e16496
smst329 <68325129+smst329@users.noreply.github.com>
2022-11-05 23:37:29 +0000

    a build

---
+++ built/precache-manifest.a919e0455469774bb84d07981f35bd90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11a1790f2a6226e23ef2b6950c5ffc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6d438ac743c9e5c4a3",
+    "url": "/static/css/main.5dbdc707.chunk.css"
+  },
+  {
+    "revision": "d822cbd4e2c21eb09f56",
+    "url": "/static/js/2.1d22e84f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1d22e84f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6d438ac743c9e5c4a3",
+    "url": "/static/js/main.7066402f.chunk.js"
+  },
+  {
+    "revision": "5b9eb446b53ba1479f2a",
+    "url": "/static/js/runtime-main.56c69188.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
